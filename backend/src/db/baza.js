a6edc9500d93ff9dac78bc219e9c8d20e8165f3a
Brunoivk <57913589+Brunoivk@users.noreply.github.com>
2020-09-16 13:09:25 +0200

    Update baza.js

--- backend/src/db/baza.js
+++ backend/src/db/baza.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-//najjednostavnije stvaranje baze, "mongodb://127.0.0.1:27017/food" zamjeni sa linkom svoje baze koju napravis na mongo atlas
+//stvaranje baze
 mongoose.connect( 'mongodb+srv://admin:admin@cluster0.1pvmq.mongodb.net/foodget?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
